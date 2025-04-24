907154c897f6f7a42c4798569f7564079270a210
Nomplex <Nompalooza@gmail.com>
2025-04-23 18:19:45 -0700

    Chapter 8 Project 15

---
+++ 08/15.c
@@ -0,0 +1,27 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char message[80];
+	int size = 0, shift;
+
+	printf("Enter message to be encrypted: ");
+	while((message[size++] = getchar()) != '\n');
+
+	printf("Enter shift amount (1-25): ");
+	scanf("%d", &shift);
+
+	// 26 letters
+	printf("Encrypted Message: ");
+	for (int i = 0; i < size; i++) {
+		if (message[i] >= 'A' && message[i] <= 'Z')
+			putchar((message[i] - 'A' + shift) % 26 + 'A');
+		else if (message[i] >= 'a' && message[i] <= 'z')
+			putchar((message[i] - 'a' + shift) % 26 + 'a');
+		else
+			putchar(message[i]);
+	}
+
+	return 0;
+}
