fc6126adfa6a969ffd9a54b1d644986b83f25bae
Nomplex <Nompalooza@gmail.com>
2025-04-26 02:35:40 -0700

    Chapter 10 Project 3

---
+++ 10/03.c
@@ -0,0 +1,150 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_RANKS 13
+#define NUM_SUITS 4
+#define NUM_CARDS 5
+
+bool straight, flush, four, three;
+int pairs;
+
+void read_cards(char hand[NUM_CARDS][2]);
+bool check_duplicate(char rank, char suit, char hand[NUM_CARDS][2], int cards_read);
+void analyze_hand(char hand[NUM_CARDS][2]);
+void print_result(void);
+
+int main(void)
+{
+	// Rank / Suit
+	char hand[NUM_CARDS][2];
+
+	for (;;) {
+		read_cards(hand);
+		analyze_hand(hand);
+		print_result();
+	}
+}
+
+void read_cards(char hand[NUM_CARDS][2])
+{
+	char ch, rank_ch, suit_ch;
+	int rank, suit;
+	bool bad_card;
+	int cards_read = 0;
+
+	while (cards_read < NUM_CARDS) {
+		bad_card = false;
+
+		printf("Enter a card: ");
+
+		rank_ch = getchar();
+		switch (rank_ch) {
+			case '0': exit(EXIT_SUCCESS);
+			case '2':		rank = 0; break;
+			case '3':		rank = 1; break;
+			case '4':		rank = 2; break;
+			case '5':		rank = 3; break;
+			case '6':		rank = 4; break;
+			case '7':		rank = 5; break;
+			case '8':		rank = 6; break;
+			case '9':		rank = 7; break;
+			case 't': case 'T':	rank = 8; break;
+			case 'j': case 'J':	rank = 9; break;
+			case 'q': case 'Q':	rank = 10; break;
+			case 'k': case 'K':	rank = 11; break;
+			case 'a': case 'A':	rank = 12; break;
+			default:		bad_card = true;
+		}
+
+		suit_ch = getchar();
+		switch (suit_ch) {
+			case 'c': case 'C':	suit = 0; break;
+			case 'd': case 'D':	suit = 1; break;
+			case 'h': case 'H':	suit = 2; break;
+			case 's': case 'S':	suit = 3; break;
+			default:		bad_card = true;
+		}
+
+		while ((ch = getchar()) != '\n')
+				if (ch != ' ') bad_card = true;
+
+		if (bad_card)
+			printf("Bad card; ignored.\n");
+		else if (check_duplicate(rank_ch, suit_ch, hand, cards_read))
+			printf("Duplicate card; ignored.\n");
+		else {
+			hand[cards_read][0] = rank_ch;
+			hand[cards_read][1] = suit_ch;
+			cards_read++;
+		}
+	}
+}
+
+bool check_duplicate(char rank, char suit, char hand[NUM_CARDS][2], int cards_read)
+{
+	for (int i = 0; i < cards_read; i++)
+		if (hand[i][0] == rank && hand[i][1] == suit)
+			return true;
+
+	return false;
+}
+
+void analyze_hand(char hand[NUM_CARDS][2])
+{
+	int num_consec = 0;
+	int rank, suit;
+
+	straight = true;
+	flush = true;
+	four = false;
+	three = false;
+	pairs = 0;
+
+	// Check for flush
+	for (int i = 1; i < NUM_CARDS; i++) {
+		if (hand[i-1][1] != hand[i][1]) {
+			suit = false;
+			break;
+		}
+	}
+
+	// Check for straight
+	for (int i = 1; i < NUM_CARDS; i++) {
+		if (hand[i-1][0] != (hand[i][0]) - 1) {
+			straight = false;
+			break;
+		}
+	}
+
+	// Check for four-of-a-kind, three-of-a-kind, pairs
+	for (int i = 0, match; i < NUM_CARDS; i++) {
+		match = 0;
+		for (int j = i + 1; j < NUM_CARDS; j++) {
+			if (hand[j][0] == hand[i][0])
+				match++;
+		}
+
+		if (match == 1) pairs++;
+		if (match == 3) three = true;
+		if (match == 4) four = true;
+	}
+}
+
+void print_result(void)
+{
+	if (straight && flush)	printf("Straight flush");
+	else if (four)		printf("Four of a kind");
+	else if (three &&
+		pairs == 1)	printf("Full house");
+	else if (flush)		printf("Flush");
+	else if (straight)	printf("Straight");
+	else if (three)		printf("Three of a kind");
+	else if (pairs == 2)	printf("Two pairs");
+	else if (pairs == 1)	printf("Pair");
+	else			printf("High card");
+
+	printf("\n\n");
+}
+
+
