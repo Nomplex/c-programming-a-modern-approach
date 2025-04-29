2530ea4e0980dd33a6dc0b8797d9cb7e446d6070
Nomplex <Nompalooza@gmail.com>
2025-04-28 18:12:29 -0700

    Chapter 15 Project 4

---
+++ 15/04/remind.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include "readline.h"
+
+#define MAX_REMIND 50
+#define MSG_LEN 60
+
+int main(void)
+{
+	char reminders[MAX_REMIND][MSG_LEN+3];
+	char day_str[12], msg_str[MSG_LEN+1];
+	int month, day, i, j, num_remind = 0, h, m;
+
+	for (;;) {
+		if (num_remind == MAX_REMIND) {
+			printf("-- No space left --\n");
+			break;
+		}
+
+		printf("Enter date (mm/dd), 24 hour time (hh:mm), and reminder: ");
+		scanf(" %2d/ %2d", &month, &day);
+		if (day == 0 || month == 0)
+			break;
+		scanf(" %2d: %2d", &h, &m);
+		sprintf(day_str, "%2.2d/%2.2d %2.2d:%2.2d", month, day, h, m);
+		read_line(msg_str, MSG_LEN);
+
+		if (month > 12 || month < 0 || day < 0 || day > 31
+		|| h > 23 || h < 0 || m > 59 || m < 0) {
+			printf("-- ERROR: Impossible Date --\n\n");
+			continue;
+		}
+
+		for (i = 0; i < num_remind; i++)
+			if (strcmp(day_str, reminders[i]) < 0)
+				break;
+		for (j = num_remind; j > i; j--)
+			strcpy(reminders[j], reminders[j-1]);
+
+		strcpy(reminders[i], day_str);
+		strcat(reminders[i], msg_str);
+
+		num_remind++;
+	}
+
+	printf("\nDay Time  Reminder\n");
+	for (i = 0; i < num_remind; i++)
+		printf(" %s\n", reminders[i]);
+
+	return 0;
+}
+
+
+
